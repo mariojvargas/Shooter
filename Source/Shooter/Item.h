9bcc310ae8546193c46c7b5d7d9cfda7c92b5050
Mario Vargas <angstrey@hotmail.com>
2022-09-10 20:19:28 -0400

    Show icon background

--- Source/Shooter/Item.h
+++ Source/Shooter/Item.h
@@ -223,6 +223,10 @@ private:
     UPROPERTY(VisibleAnywhere, Category = "Item Properties", meta = (AllowPrivateAccess = "true"))
     float FresnelReflectFraction;
 
+    /** Background of this item in the inventory */
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Inventory", meta = (AllowPrivateAccess = "true"))
+    UTexture2D* IconBackground{ nullptr };
+
 public:
 	FORCEINLINE UWidgetComponent* GetPickupWidget() const 
 	{
