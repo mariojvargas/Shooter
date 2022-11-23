d77341fb93a6d3c2f2650e8cff561615059bc9d0
Mario Vargas <angstrey@hotmail.com>
2022-11-23 17:45:05 -0500

    Animate enemy running state

--- Source/Shooter/GruxAnimInstance.h
+++ Source/Shooter/GruxAnimInstance.h
@@ -13,5 +13,16 @@ UCLASS()
 class SHOOTER_API UGruxAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void UpdateAnimationProperties(float DeltaTime);
+
+private:
+    /** Lateral movement speed */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+    float Speed;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    class AEnemy* Enemy{ nullptr };
 };
